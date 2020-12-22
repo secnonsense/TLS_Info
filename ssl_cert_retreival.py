c149314a0e214aeba7cab1cf8ccea8a37cecf8fe
secnonsense <27748027+secnonsense@users.noreply.github.com>
2020-12-22 10:00:10 -0500

    Update ssl_cert_retreival.py

--- ssl_cert_retreival.py
+++ ssl_cert_retreival.py
@@ -1,23 +1,23 @@
 import ssl, socket
 
-file=open("500.txt","r")
+file=open("input.txt","r")
 output=open("output.txt", "w")
 hosts = file.readlines() 
 for host in hosts:
   try:
     hostname=host.strip()
-    ctx = ssl.create_default_context()
-    with ctx.wrap_socket(socket.socket(), server_hostname=hostname) as s:
-        s.settimeout(4)
-        s.connect((hostname, 443))
-        cert = s.getpeercert()
+    context = ssl.create_default_context()
+    with context.wrap_socket(socket.socket(), server_hostname=hostname) as sock:
+        sock.settimeout(4)
+        sock.connect((hostname, 443))
+        cert = sock.getpeercert()
 
     #subject = dict(x[0] for x in cert['subject'])
     #issued_to = subject['commonName']
     issuer = dict(x[0] for x in cert['issuer'])
     issued_by = issuer['commonName']
     output.write("Site: " + hostname + " - issuer: " + issuer['organizationName'] + "\n")
-    print("Site: " + hostname + " - issuer: " + issuer['organizationName'])
+    print("Site: " + hostname + " - Issuer: " + issuer['organizationName'])
   except:
     continue
 output.close()
